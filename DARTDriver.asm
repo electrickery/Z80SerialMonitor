433ace7a54d567fa577f16a4cb0490badf007477
Fred Jan Kraan <fjkraan@electrickery.nl>
2022-01-25 13:16:58 +0100

    Cleanup code, removed unused CTC initialization.

--- DARTDriver.asm
+++ DARTDriver.asm
@@ -7,130 +7,117 @@
 ;  CREATE DATE :	2020-12-25
 ;***************************************************************************
 
-;The addresses that the CTC & DART resides in I/O space.
+;The addresses of the DART in I/O space.
 ;Change to suit hardware.
-CTCH0:    EQU     040H
-CTCH1:    EQU     041H
-CTCH2:    EQU     042H
-CTCH3:    EQU     043H
 
-DRTDA:   EQU     060H
-DRTDB:   EQU     061H
-DRTCA:   EQU     062H
-DRTCB:   EQU     063H
+DRTDA:  EQU     UART_BASE   ;   060h
+DRTDB:  EQU     DRTDA + 1
+DRTCA:  EQU     DRTDA + 2
+DRTCB:  EQU     DRTDA + 3
 
 ; useful patterns
-TXBUFEMPTY:     EQU     000000100b
-RXCHARAVL:      EQU     000000001b
+TXBUFEMPTY:  EQU     000000100b
+RXCHARAVL:   EQU     000000001b
+; unused
+XOFF:   EQU     013h
+XON:    EQU     015h
 
 ;***************************************************************************
 ;UART_INIT
-;Function: Initialize the UART to BAUD Rate 9600 (1.8432 MHz clock input)
+;Function: Initialize the UART to 1x BAUD Rate clock, 
+; 8 bits, 1 stop bit, no parity, enable Tx and Rx
 ;***************************************************************************
 UART_INIT:
-CTC_INIT:  LD A, 00000011B ; int off, timer on, prescaler=16, don't care ext. TRG edge,
-                                ; start timer on loading constant, no time constant follows
-                                ; sw­rst active, this is a ctrl cmd
-           OUT (CTCH0),A     ; CH0 is on hold now
-           OUT (CTCH1),A     ; CH1 is on hold now
-           OUT (CTCH3),A     ; CH3 is on hold now
+        LD      HL, DRTTB
+        LD      C, DRTCB
+        LD      B, E_DRTTB - DRTTB
+        OTIR                    ; write 8 bytes from DRTTB to port DRTCB
+        RET
 
-DARTB_INIT:
-	       LD A, 00000111B ; int off, timer, prescaler=16, any edge,
-                                ;  load trigger, constant following, reset, control word next
-BADDRATE:  OUT (CTCH2), A
-           LD A, 12        ; closest integer divider to 11.652164714
-           OUT (CTCH2), A
-                
-DART_INIT:
-INIT:   	LD      HL, DRTTB
-       	 	LD      C, DRTCB
-        	LD      B, 8
-        	OTIR                    ; write 8 bytes from DRTTB to port DRTCB
-            RET
+DRTTB:                      ; alternate register select and values (not for WR0)
+        DEFB   00011000b    ; DRTWR0 - Register pointer & modes. Channel reset.
+        DEFB   00000100b    ; DRTWR4 - Tx/Rx misc. params & modes
+;        DEFB   01000100b    ;  X16+STOP1+NOPARITY
+        DEFB   00000100b    ;   X1+STOP1+NOPARITY
+;        DEFB   00001100b    ;  X1+STOP2+NOPARITY:
+        DEFB   00000101b    ; DRTWR5 - Tx params & controls
+        DEFB   01101000b    ;   TX8+TXEN
+        DEFB   00000011b    ; DRTWR3 - Rx params & controls
+        DEFB   11000001b    ;   RX8+RXEN
+E_DRTTB:
 
-DRTTB:
-DRTWR0:            DEFB   00000000b
-CHRES:             DEFB   00011100b
-DRTWR4:            DEFB   00000100b
-;X16+STOP1+NOPARITY: DEFB  01000100b
-X1+STOP1+NOPARITY: DEFB   00000100b
-;X1+STOP2+NOPARITY: DEFB   00001100b
-DRTWR5:            DEFB   00000101b
-TX8+TXEN:          DEFB   01101000b
-DRTWR3:            DEFB   00000011b
-RX8+RXEN:          DEFB   11000001b            
-                	
 ;***************************************************************************
 ;UART_PRNT_STR:
-;Function: Print out string starting at MEM location (HL) to 16550 UART
+;Function: Print out string starting at MEM location (HL) to Z80 DART
 ;***************************************************************************
 UART_PRNT_STR:
-			PUSH	AF
+        PUSH    AF
 UARTPRNTSTRLP:
-			LD		A,(HL)
-            CP		EOS					;Test for end byte
-            JP		Z,UART_END_PRNT_STR	;Jump if end byte is found
-			CALL	UART_TX
-            INC		HL					;Increment pointer to next char
-            JP		UARTPRNTSTRLP	;Transmit loop
+        LD      A,(HL)
+        CP      EOS					;Test for end byte
+        JP      Z,UART_END_PRNT_STR	;Jump if end byte is found
+        CALL    UART_TX
+        INC     HL					;Increment pointer to next char
+        JP      UARTPRNTSTRLP	    ;Transmit loop
 UART_END_PRNT_STR:
-			POP		AF
-			RET	 
-			 	
+        POP     AF
+        RET
+
 ;***************************************************************************
 ;UART_TX_READY
 ;Function: Check if UART is ready to transmit
 ;***************************************************************************
 UART_TX_RDY:
-WAITTXRDY:  IN      A, (DRTCB)		; 
-            AND     TXBUFEMPTY		; Can we send the next char?
-            JR      Z, WAITTXRDY	; If not, wait
-            RET
-	
+WAITTXRDY:
+        IN      A, (DRTCB)		; 
+        AND     TXBUFEMPTY		; Can we send the next char?
+        JR      Z, WAITTXRDY	; If not, wait
+        RET
+
 ;***************************************************************************
 ;UART_TX_SEND
-;Function: Transmit character in A to UART
+;Function: Transmit character in A to DART
 ;***************************************************************************
-
 TX_SEND:
-			OUT     (DRTDB), A		; Out it goes!
-			RET
+        OUT     (DRTDB), A		; Out it goes!
+        RET
 
 ;***************************************************************************
 ; UART_TX
 ; Function: Blocking TX routine
 ;***************************************************************************
 UART_TX:
-			PUSH	AF
-			CALL	WAITTXRDY
-			POP		AF
-			CALL	TX_SEND
-			RET
+        PUSH    AF
+        CALL    UART_TX_RDY
+        POP     AF
+        CALL    TX_SEND
+        RET
 
 ;***************************************************************************
 ;UART_RX_READY
 ;Function: Check if UART is ready to receive - blocking receive check
 ;***************************************************************************
 UART_RX_RDY:
-RX_NOT_RDY:	CALL	RX_CHK
-            JR		Z, RX_NOT_RDY		; If not, wait
-			RET
+RX_NOT_RDY:
+        CALL    RX_CHK
+        JR      Z, RX_NOT_RDY		; If not, wait
+        RET
 
 ;***************************************************************************
 ; DART_RX_CHK
 ; Function: Non-blocking receive check		
 ;***************************************************************************
 RX_CHK:
-			IN      A, (DRTCB)		; 
-            AND     RXCHARAVL		; Has some char arrived?
-			RET			
-	
+        IN      A, (DRTCB)		; 
+        AND     RXCHARAVL		; Has some char arrived?
+        RET
+
 ;***************************************************************************
 ;UART_RX
 ;Function: Receive character in UART to A; wait for char
 ;***************************************************************************
 UART_RX:
-			CALL	UART_RX_RDY		; Make sure UART is ready to receive
-            IN		A, (DRTDB)		; Get it
-			RET			
+        CALL    UART_RX_RDY		; Make sure UART is ready to receive
+        IN      A, (DRTDB)		; Get it
+        RET
+        
