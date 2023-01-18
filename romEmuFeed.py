1fb8d6285ea465c23891acb33e0e7cef42c71ebf
F.J. Kraan <fjkraan@electrickery.nl>
2023-01-18 10:49:50 +0100

    Added proper argument parser, allowing selection of serial port.

--- romEmuFeed.py
+++ romEmuFeed.py
@@ -4,18 +4,58 @@
 import sys
 import serial
 import time
+import argparse
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=2)  # open serial port
+port = '/dev/ttyACM0'
+baud = 9600
+parser = argparse.ArgumentParser(description='Send hex-intel file to ROM Emulator.')
+parser.add_argument('-p', '--port', type=str, nargs='?',
+                    help='Serial port to use (9600 BAUD)')
+parser.add_argument('-x', '--hexFile', type=str, nargs='?',
+                    help='hex-Intel file')
+parser.add_argument('-o', '--offset', type=str, nargs='?',
+                    help='offset to target address')               
+
+args = parser.parse_args()
+
+if (args.port):
+    port = args.port
+    
+if (args.offset):
+    offset = "F" + args.offset
+else:
+    offset = 0
+
+if (args.hexFile):
+    hexFile = args.hexFile
+else:
+    print("Usage: python3 romEmuFeed.py  -x HEXFILE [-p PORT] [-o OFFSET]")
+    print("       default port is " + port + ", default offset is " + hex(offset))
+    quit(1)
+
+
+
+
+
+
+
+
+
+ser = serial.Serial(port, baud, timeout=2)  # open serial port
 LF = "\r\n"
 hexOffsetStr = ""
 sendDelay = 0.05
 time.sleep(sendDelay)
 print(ser.readline())
 
-if len(sys.argv) > 1:
-    hexFile = sys.argv[1]
-if len(sys.argv) > 2:
-    hexOffsetStr = "F" + sys.argv[2]
+#if len(sys.argv) <= 1:
+#    print("Usage: python3 romEmuFeed.py hexFile [hexOffset]")
+#    print("       port is " + port + " speed is " + str(baud))
+#    quit()
+#else:
+#    hexFile = sys.argv[1]
+#if len(sys.argv) > 2:
+#    hexOffsetStr = "F" + sys.argv[2]
 
 file = open(hexFile, 'r')
 lines = file.readlines()
